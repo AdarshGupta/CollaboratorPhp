aed7fba497848581d30b8141f86a25dab6897bf4
AdarshGupta <adarsh035@gmail.com>
2017-10-11 12:33:31 +0530

    Route Model binding

--- app/Http/Controllers/ProblemsController.php
+++ app/Http/Controllers/ProblemsController.php
@@ -13,9 +13,10 @@ class ProblemsController extends Controller
     	return view('problems.index', compact('problems'));
     }
 
-    public function show($id)
+    public function show(Problem $problem) 
     {
-    	$problem = Problem::find($id);
+    	//// The variable name $problem should match with wildcard in the route in web.php (URI segment) or else it won't work. It does autom. Tfinds by ID matching problem
+    	//$problem = Problem::find($id);
     	return view('problems.show', compact('problem'));
     }
 }
